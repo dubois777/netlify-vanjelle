849747ad5ea82e1a4e2b97c8860bc83027ef0afa
dubois777 <125222339+dubois777@users.noreply.github.com>
2023-10-08 03:11:03 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const cookieBox =  document.querySelector('.wrapper'),
+buttons =  document.querySelector('.wrapper');
+
+const executeCodes = () => {
+cookieBox.classList.add("show")
+};
+
+
+window.addEventListener("load", executeCodes);
