0055b77b6d5c4f0e855c131b19c3ddfa6e48ee28
dubois777 <125222339+dubois777@users.noreply.github.com>
2023-10-08 03:21:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const cookieBox =  document.querySelector('.wrapper'),
 buttons =  document.querySelectorAll('.button');
 
 const executeCodes = () => {
+  if(document.cookie.includes("dmd")) return;
 cookieBox.classList.add("show")
 
   buttons.forEach(button => {
     button.addEventListener("click", () => {
       cookieBox.classList.remove("show");
+
+      if(button.id == "button") {
+       document.cookie = "cookieBy = dmd; max-age=" + 60*60*24*30;
     })
   })
 };
